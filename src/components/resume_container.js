e82b9d8802be26fa0da9b208d0aeeb7eeee1a54a
qsmalljr <qmanjr123@gmail.com>
2020-06-19 19:43:15 -0400

    wip

---
+++ src/components/resume_container.js
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+import React from "react";
+import "./resume_container.css";
+
+const ResumeContainer = () => {
+    return (
+        <>
+            <p>Quentin smalllllllll</p>
+        </>
+    );
+};
+
+ResumeContainer.propTypes = {
+};
+
+ResumeContainer.defaultProps = {
+};
+
+export default ResumeContainer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 import LeftPanel from "../components/left_panel";
+import ResumeContainer from "../components/resume_container";
 
 const IndexPage = () => (
     <Layout>
         <SEO title="Home" />
         <LeftPanel />
+        <ResumeContainer />
     </Layout>
 );
 
